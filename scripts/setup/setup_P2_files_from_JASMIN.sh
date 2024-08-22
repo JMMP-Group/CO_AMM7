a9e33799a2562633ddae1433b0abb78efd2ff92f
jpolton <jelt@noc.ac.uk>
2024-08-22 20:40:20 +0100

    Update setup_P2_files_from_JASMIN.sh

--- scripts/setup/setup_P2_files_from_JASMIN.sh
+++ scripts/setup/setup_P2_files_from_JASMIN.sh
@@ -3,9 +3,9 @@
 # domain_cfg.nc
 wget -O domain_cfg.nc https://gws-access.jasmin.ac.uk/public/jmmp/AMM7/CO9_repo/domain_cfg_co9amm7_MEsL51r10-07.nc
 
-# bdy coordinate files
-wget http://gws-access.jasmin.ac.uk/public/jmmp/AMM7/grid/coordinates.bdy.nc
-wget http://gws-access.jasmin.ac.uk/public/jmmp/AMM7/grid/coordinates.skagbdy.nc
+# bdy coordinate files (to be updated)
+#wget http://gws-access.jasmin.ac.uk/public/jmmp/AMM7/grid/coordinates.bdy.nc
+#wget http://gws-access.jasmin.ac.uk/public/jmmp/AMM7/grid/coordinates.skagbdy.nc
 
 
 
@@ -16,8 +16,8 @@ wget http://gws-access.jasmin.ac.uk/public/jmmp/AMM7/grid/coordinates.skagbdy.nc
 wget -r -nH -e robots=off --cut-dirs=6 --no-parent --reject="index.html*" https://gws-access.jasmin.ac.uk/public/jmmp/AMM7/inputs/TIDE/ORGINAL/
 mv ORGINAL TIDE
 
-# initial condition
-wget -P IC https://gws-access.jasmin.ac.uk/public/jmmp/AMM7/inputs/IC/initcd_y2005m01.nc
+# initial condition (to be updated)
+#wget -P IC https://gws-access.jasmin.ac.uk/public/jmmp/AMM7/inputs/IC/initcd_y2005m01.nc
 
 #SBC
 mkdir SBC
@@ -25,13 +25,13 @@ wget -P SBC -e robots=off -r -nd --no-parent -A '*2005*nc' https://gws-access.ja
 wget -P SBC https://gws-access.jasmin.ac.uk/public/jmmp/AMM7/inputs/SBC/ERA5_LSM.nc
 wget -P SBC https://gws-access.jasmin.ac.uk/public/jmmp/AMM7/inputs/SBC/weights_era5_bicubic.nc
 
-#BDY
-mkdir BDY
-wget -P BDY -e robots=off -r -nd --no-parent --reject="index.html*" https://gws-access.jasmin.ac.uk/public/jmmp/AMM7/inputs/BDY/2005/
+#BDY (to be updated)
+#mkdir BDY
+#wget -P BDY -e robots=off -r -nd --no-parent --reject="index.html*" https://gws-access.jasmin.ac.uk/public/jmmp/AMM7/inputs/BDY/2005/
 
-#BDY_SKAG
-mkdir BDY_SKAG
-wget -P BDY_SKAG -e robots=off -r -nd --no-parent --reject="index.html*" https://gws-access.jasmin.ac.uk/public/jmmp/AMM7/inputs/BDY_SKAG/2005/
+#BDY_SKAG (to be updated)
+#mkdir BDY_SKAG
+#wget -P BDY_SKAG -e robots=off -r -nd --no-parent --reject="index.html*" https://gws-access.jasmin.ac.uk/public/jmmp/AMM7/inputs/BDY_SKAG/2005/
 
 
 
